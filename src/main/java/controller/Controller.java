31b808f58616bd9a853ede1c3d386808ce30e30e
diogooaraujo <diogosiqueira.info2@gmail.com>
2022-10-24 21:49:47 -0300

    Conexão com o Banco de Dados Estabelecida

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -6,14 +6,15 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import model.DAO; // Importada a classe DAO
 /**
  * Servlet implementation class Controller
  */
 @WebServlet(urlPatterns = {"/Controller", "/main"})
+// Adicionado o parâmetro ("urlPatterns = { ... "/main"})
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+    DAO dao = new DAO(); // Criando o objeto DAO.
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -28,6 +29,8 @@ public class Controller extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// Teste de Conexão
+		dao.testeConexao(); // O método para testeConexao() foi utilizado com o objeto "dao", ele irá imprimir a String de conexão se tudo estiver OK. Se ocorrer algum problema, mostrará qual erro ocorreu.
 	}
 
 }
--- src/main/java/model/DAO.java
+++ src/main/java/model/DAO.java
@@ -1,5 +1,41 @@
 package model;
 
-public class DAO {
+import java.sql.Connection; // JDBC
+import java.sql.DriverManager;
 
+public class DAO {
+	/** Módulo de Conexão **/
+	// Parâmetros de Conexão
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://127.0.0.1:3306/dbagenda?useTimezone=true&serverTimezone=UTC"; // Informando o IP do Servidor, Nome do Banco de Dados e o Fuso Horário de referência universal.
+	private String user = "root"; // Usuário do Banco de Dados
+	private String password = "31102013@DBOnline!"; // Senha do Banco de Dados
+	// Método de Conexão
+	private Connection conectar() { 
+		// Criada a função conectar, da classe Connection, que faz parte do JDBC. 
+		Connection con = null; 
+		// Criado o objeto de nome "con", com o valor nulo. Será utilizado para estabelecer uma sessão com o banco de dados. 
+		
+		// É necessário utilizar o try catch para realizar os tratamentos de exceção, caso ocorra algum erro:
+		try {
+			Class.forName(driver); // O método forName irá ler o driver do banco de dados da variável que foi criada na linha 8, e verificará se a escrita está correta.
+			con = DriverManager.getConnection(url, user, password); // O objeto con é utilizado para estabelecer uma conexão com o banco de dados. A classe DriverManager irá gerenciar o driver. O método "getConnection() irá obter os parâmetros de conexão. URL, USER e PASSWORD são variáveis definidas anteriormente.
+			return con; // Retornando se a conexão foi bem sucedida.
+		} catch (Exception e) {
+			System.out.println(e); // Para saber qual exceção ocorreu. 
+			return null;
+		}
+	}
+	// Teste de Conexão
+	public void testeConexao() {
+		try {
+			// Usando como modelo a classe Connection, foi criado o objeto con, que invoca/executa o método conectar.
+			Connection con = conectar();
+			System.out.println(con);
+			con.close();
+		} catch (Exception e) {
+			// Caso ocorra alguma exceção, informar qual foi.
+			System.out.println(e);
+		}
+	}
 }
